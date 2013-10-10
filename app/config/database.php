99fe262b1ea696c4fa122c4123d0894cbae394c2
Shawn Tunney <shawn.tunney@gmail.com>
2013-10-10 09:30:44 -0400

    Tweaking redis

--- app/config/database.php
+++ app/config/database.php
@@ -80,10 +80,10 @@ return array(
 
 	'redis' => array(
 
-		'cluster' => true,
+		'cluster' => false,
 
 		'default' => array(
-			'host'     => '127.0.0.1',
+			'host'     => 'tunnel.pagodabox.com',
 			'port'     => 6379,
 			'database' => 0,
 		),
